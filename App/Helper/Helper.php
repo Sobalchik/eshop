343a1df94bcc5546767bbc225951aaafd9ecc3c1
Грошев Максим <an9ous8@mail.ru>
2022-02-02 18:13:46 +0200

    Helper singleton

--- App/Helper/Helper.php
+++ App/Helper/Helper.php
@@ -1,6 +1,22 @@
 <?php
 
+namespace App\Helper;
+
 class Helper
 {
+
+	private static $instance;
+
+	public static function getInstance()
+	{
+		if(self::$instance)
+		{
+			return  self::$instance;
+		}
+
+		self::$instance = new self();
+
+		return self::$instance;
+	}
 
 }
