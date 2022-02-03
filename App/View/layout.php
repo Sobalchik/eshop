993a7f7dceef002c1415718ea6b2386eea52acc8
maik791277 <67161648+maik791277@users.noreply.github.com>
2022-02-03 22:10:48 +0200

    Добавлен layout.php

--- App/View/layout.php
+++ App/View/layout.php
@@ -1,38 +1,40 @@
 <?php
 
+/** @var array $excursions */
+
+var_dump($excursions);
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Title</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Title</title>
 
-    <link rel="stylesheet" type="text/css" href="../../Public/Resources/CSS/Reset.css">
-    <link rel="stylesheet" type="text/css" href="../../Public/Resources/CSS/style.css">
+	<link rel="stylesheet" type="text/css" href="../../Public/Resources/CSS/Reset.css">
+	<link rel="stylesheet" type="text/css" href="../../Public/Resources/CSS/style.css">
 </head>
 <body>
 <div class="header">
-    <div class="menu">
-        <div class="bloc-logo">
-            <img class="header-logo" src="../../Public/Resources/Images/logo_template14%201.png">
-        </div>
-        <div class="bloc-menu">
-            <a class="menu-button">Экскурсии</a>
-            <a class="menu-button">Клиентам</a>
-            <a class="menu-button">Блог</a>
-            <a class="menu-button">Отзывы</a>
-            <a class="menu-button">О нас</a>
-        </div>
+	<div class="menu">
+		<div class="bloc-logo">
+			<img class="header-logo" src="../../Public/Resources/Images/logo_template14%201.png">
+		</div>
+		<div class="bloc-menu">
+			<a class="menu-button">Экскурсии</a>
+			<a class="menu-button">Клиентам</a>
+			<a class="menu-button">Блог</a>
+			<a class="menu-button">Отзывы</a>
+			<a class="menu-button">О нас</a>
+		</div>
 		<div class="exit-log">
 			<img src="../../Public/Resources/Images/1.png">
 			<a>Личный кабинет</a>
 		</div>
-    </div>
+	</div>
 </div>
 <div class="content">
 	/* $content */
