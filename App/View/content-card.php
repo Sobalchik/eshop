5a660451f0f2bcc5dd241c56a14c19f21a4e20a3
maik791277 <67161648+maik791277@users.noreply.github.com>
2022-02-03 22:07:30 +0200

    Добавлен layout.php

---
+++ App/View/content-card.php
@@ -0,0 +1,34 @@
+<?php
+?>
+
+<div class="bloc-2-box">
+	<div class="asd">
+		<div class="box-date">
+			<p class="box-date-num">05</p>
+			<p class="box-date-text">Марта</p>
+		</div>
+		<div class="box-h1">
+			<p class="box-h1-1">Буэнос-айрес</p>
+			<p class="box-h1-2">Аргентина</p>
+		</div>
+		<div class="box-bottom">
+			<img src="../../Public/Resources/Images/солнечно%201.png">
+			<p class="box-bottom-weather">24°C</p>
+			<p class="box-bottom-many">₽ 53 000</p>
+		</div>
+	</div>
+	<div class="overlay">
+		<div class="overlay-like">
+			<a><img src="../../Public/Resources/Images/2961957%201.svg"></a>
+		</div>
+		<div class="loc">
+			<p class="overlay-progress-text">asd</p>
+			<div class="progress">
+				<div class="progress-value" style="width: 30%;"></div>
+			</div>
+		</div>
+		<div class="overlay-detailed">
+			<a>Подробнее<img src="../../Public/Resources/Images/2989988%201.svg"></a>
+		</div>
+	</div>
+</div>
