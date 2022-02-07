534229ff6062a9ad46bfd58c3a3cc70d91fecddb
semibratov <Q!w2e3r4t5>
2022-02-07 14:18:37 +0200

    Получение данных из таблицы миграции и получение названий файлов из папки миграции

--- App/Database/Migration.php
+++ App/Database/Migration.php
@@ -8,29 +8,47 @@ class Migration
 {
 	public $table = 'up_migration';
 	public $path = 'Migrations';
-	protected $db;
+
+	private $pathDir;
+	private $db;
 
 	public function __construct(mysqli $db)
 	{
+		$this->pathDir = str_replace('\\', '/', realpath(dirname(__FILE__)) . '/');
 		$this->db = $db;
 		$this->checkEnvironment();
 	}
 
 	public function checkEnvironment()
 	{
-		if (!file_exists(str_replace('\\', '/', realpath(dirname(__FILE__)) . '/').$this->path))
+		if (!file_exists($this->pathDir.$this->path))
 		{
-			mkdir(str_replace('\\', '/', realpath(dirname(__FILE__)) . '/').$this->path);
+			mkdir($this->pathDir.$this->path);
 		}
 		$this->executeDB('CREATE TABLE IF NOT EXISTS ' . $this->table . ' (`ID` int not null auto_increment, `FILE_MIGRATION` varchar(500) not null, PRIMARY KEY (ID)) ENGINE=MyISAM DEFAULT CHARSET=utf8;');
 	}
 
-	public function getMigrationFiles()
+	public function up()
 	{
-		//
+		$this->getMigrationFiles();
 	}
 
-	public function up()
+	private function getMigrationFiles()
+	{
+		$fileMigrationListInFolder = glob($this->pathDir.$this->path. '*.sql');
+		print_r($fileMigrationListInFolder);
+		$fileMigrationListInBD = $this->queryDB('SELECT FILE_MIGRATION FROM up_migration;');
+		if (!$fileMigrationListInBD)
+		{
+
+		}
+		else
+		{
+
+		}
+	}
+
+	private function executeCommandShell()
 	{
 		//
 	}
@@ -38,5 +56,24 @@ class Migration
 	private function executeDB($query)
 	{
 		$result = mysqli_query($this->db, $query);
+		if (!$result)
+		{
+			trigger_error(mysqli_error($this->db), E_USER_ERROR);
+		}
+	}
+
+	private function queryDB($query): array
+	{
+		$result = mysqli_query($this->db, $query);
+		if (!$result)
+		{
+			trigger_error(mysqli_error($this->db), E_USER_ERROR);
+		}
+		$fileMigrationList = [];
+		while ($fileMigration = mysqli_fetch_assoc($result))
+		{
+			$fileMigrationList[]=$fileMigration['FILE_MIGRATION'];
+		}
+		return $fileMigrationList;
 	}
 }
