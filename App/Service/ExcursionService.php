43251c60b4dcafe37b0e33074333fff42b173de1
willow-witch <sealana048@gmail.com>
2022-02-09 12:11:44 +0200

    ExcursionService.php: реализован методы сортировки экскурсий по цене(по возрастанию и убыванию) и рейтингу(по убыванию)

--- App/Service/ExcursionService.php
+++ App/Service/ExcursionService.php
@@ -135,6 +135,192 @@ class ExcursionService
 	}
 
 
+	public static function sortExcursionsByPriceAsc(mysqli $db, array $idList) : array
+	{
+		$query = "
+			select
+				ID as 'id',
+				NAME_CITY as 'nameCity',
+				NAME_COUNTRY as 'nameCountry',
+				DATE_TRAVEL as 'dateTravel',
+				PRICE as 'price',
+				INTERNET_RATING as 'internetRating',
+				ENTERTAINMENT_RATING as 'entertainmentRating',
+				SERVICE_RATING as 'serviceRating',
+				RATING as 'rating',
+				DEGREES as 'degrees',
+				ACTIVE as 'active',
+				(
+					select
+						up_image.PATH
+					from up_product_image
+							 left join up_image on up_product_image.IMAGE_ID = up_image.ID
+					where up_product_image.PRODUCT_ID = up_product.ID
+					  and up_image.MAIN = '1'
+				) as 'imageList'
+			from up_product 
+			where up_product.ID in 
+			      (" . implode(',', array_map('intval', $idList)) . ")
+			order by up_product.PRICE;
+		";
+
+		$result = mysqli_query($db, $query);
+
+		if (!$result)
+		{
+			trigger_error(mysqli_error($db), E_USER_ERROR);
+		}
+
+		$excursions = [];
+
+		while ($excursion = mysqli_fetch_assoc($result))
+		{
+			$excursions[] = new Excursion(
+				$excursion['id'],
+				$excursion['nameCity'],
+				$excursion['nameCountry'],
+				$excursion['dateTravel'],
+				$excursion['price'],
+				'',
+				$excursion['internetRating'],
+				$excursion['entertainmentRating'],
+				$excursion['serviceRating'],
+				$excursion['rating'],
+				$excursion['degrees'],
+				$excursion['active'],
+				'',
+				'',
+				$excursion['imageList']
+			);
+		}
+
+		return $excursions;
+	}
+
+	public static function sortExcursionsByPriceDesc(mysqli $db, array $idList) : array
+	{
+		$query = "
+			select
+				ID as 'id',
+				NAME_CITY as 'nameCity',
+				NAME_COUNTRY as 'nameCountry',
+				DATE_TRAVEL as 'dateTravel',
+				PRICE as 'price',
+				INTERNET_RATING as 'internetRating',
+				ENTERTAINMENT_RATING as 'entertainmentRating',
+				SERVICE_RATING as 'serviceRating',
+				RATING as 'rating',
+				DEGREES as 'degrees',
+				ACTIVE as 'active',
+				(
+					select
+						up_image.PATH
+					from up_product_image
+							 left join up_image on up_product_image.IMAGE_ID = up_image.ID
+					where up_product_image.PRODUCT_ID = up_product.ID
+					  and up_image.MAIN = '1'
+				) as 'imageList'
+			from up_product 
+			where up_product.ID in 
+			      (" . implode(',', array_map('intval', $idList)) . ")
+			order by up_product.PRICE DESC;
+		";
+
+		$result = mysqli_query($db, $query);
+
+		if (!$result)
+		{
+			trigger_error(mysqli_error($db), E_USER_ERROR);
+		}
+
+		$excursions = [];
+
+		while ($excursion = mysqli_fetch_assoc($result))
+		{
+			$excursions[] = new Excursion(
+				$excursion['id'],
+				$excursion['nameCity'],
+				$excursion['nameCountry'],
+				$excursion['dateTravel'],
+				$excursion['price'],
+				'',
+				$excursion['internetRating'],
+				$excursion['entertainmentRating'],
+				$excursion['serviceRating'],
+				$excursion['rating'],
+				$excursion['degrees'],
+				$excursion['active'],
+				'',
+				'',
+				$excursion['imageList']
+			);
+		}
+
+		return $excursions;
+	}
+
+	public static function sortExcursionsByRatingDesc(mysqli $db, array $idList) : array
+	{
+		$query = "
+			select
+				ID as 'id',
+				NAME_CITY as 'nameCity',
+				NAME_COUNTRY as 'nameCountry',
+				DATE_TRAVEL as 'dateTravel',
+				PRICE as 'price',
+				INTERNET_RATING as 'internetRating',
+				ENTERTAINMENT_RATING as 'entertainmentRating',
+				SERVICE_RATING as 'serviceRating',
+				RATING as 'rating',
+				DEGREES as 'degrees',
+				ACTIVE as 'active',
+				(
+					select
+						up_image.PATH
+					from up_product_image
+							 left join up_image on up_product_image.IMAGE_ID = up_image.ID
+					where up_product_image.PRODUCT_ID = up_product.ID
+					  and up_image.MAIN = '1'
+				) as 'imageList'
+			from up_product 
+			where up_product.ID in 
+			      (" . implode(',', array_map('intval', $idList)) . ")
+			order by up_product.RATING DESC;
+		";
+
+		$result = mysqli_query($db, $query);
+
+		if (!$result)
+		{
+			trigger_error(mysqli_error($db), E_USER_ERROR);
+		}
+
+		$excursions = [];
+
+		while ($excursion = mysqli_fetch_assoc($result))
+		{
+			$excursions[] = new Excursion(
+				$excursion['id'],
+				$excursion['nameCity'],
+				$excursion['nameCountry'],
+				$excursion['dateTravel'],
+				$excursion['price'],
+				'',
+				$excursion['internetRating'],
+				$excursion['entertainmentRating'],
+				$excursion['serviceRating'],
+				$excursion['rating'],
+				$excursion['degrees'],
+				$excursion['active'],
+				'',
+				'',
+				$excursion['imageList']
+			);
+		}
+
+		return $excursions;
+	}
+
 	public static function getExcursionsByTag(): array
 	{
 		return [];
