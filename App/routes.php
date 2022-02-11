efe4fbdde596fa014020de7716689acd73c5a6ca
Krupin Mikhail <krupin732@yandex.ru>
2022-02-11 11:29:21 +0200

    Переписаны пути. Теперь основаная страница открываеться по пути eshop

--- App/routes.php
+++ App/routes.php
@@ -2,13 +2,13 @@
 
 \App\Lib\Router::add(
 	"GET",
-	"/home",
+	"/",
 	[\App\Controller\MainController::class, 'showTopExcursions']
 );
+
 
 \App\Lib\Router::add(
 	"GET",
 	"/placeholder",
 	[\App\Controller\MainController::class, 'showPlaceHolder']
 );
-
