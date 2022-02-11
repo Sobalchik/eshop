503da5e8a21c6575e5ee465ecc392cdeeef91152
Krupin Mikhail <krupin732@yandex.ru>
2022-02-11 11:30:59 +0200

    Перенос из index.php миграций и подключение базы данных в Application.php

--- App/Lib/Application.php
+++ App/Lib/Application.php
@@ -2,68 +2,27 @@
 
 namespace App\Lib;
 
+use App\Config\Database;
+use App\Config\Settings;
+use App\Database\Migration;
 use Exception;
 
 class Application
 {
 	public static function run(): ?Response
 	{
-		$callback = \App\Lib\Router::route($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+		$settings = Settings::getInstance();
+		$database = Database::getInstance();
 
-		if ($callback === null)
+		if($settings->isDev())
 		{
-			return Response::error(404, "Not Found");
-		}
-
-		$params = $callback['params'];
-
-		if (is_array($callback['callback']))
-		{
-			$callbackReflection = new \ReflectionMethod($callback['callback'][0], $callback['callback'][1]);
-		}
-		elseif (is_string($callback['callback']) || is_callable($callback['callback']))
-		{
-			$callbackReflection = new \ReflectionFunction($callback['callback']);
+			$migration = new Migration($database->connect());
+			$migration->up();
 		}
 
-		$args = [];
-		foreach ($callbackReflection->getParameters() as $parameter)
-		{
-			if (isset($params[$parameter->getName()]))
-			{
-				$args[] = $params[$parameter->getName()];
-			}
-			else
-			{
-				if (!$parameter->isDefaultValueAvailable())
-				{
-					throw new \Exception("No value for parameter $" . $parameter->getName());
-				}
-			}
-		}
 
-		try
-		{
-			$result = call_user_func_array($callback['callback'], $args);
-		}
-		catch (Exception $e)
-		{
-			// TODO: журналируем и выводим исключение
-			return null;
-		}
-		if ($result instanceof Response)
-		{
-			return $result;
-		}
-		if (is_array($result))
-		{
-			return Response::json($result);
-		}
-		if (is_string($result))
-		{
-			return Response::text($result);
-		}
 
-		return Response::error('404', 'Not Found');
+
+		return Router::route($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
 	}
 }
--- Public/index.php
+++ Public/index.php
@@ -5,12 +5,9 @@ use App\Config\Database;
 require "../App/autoload.php";
 require '../App/routes.php';
 
-$db = App\Config\Database::getInstance();
-$db->connect();
-
-#$migration = new \App\Database\Migration(Database::getInstance()->connect());
-#$migration->up();
 
 
 $response = App\Lib\Application::run();
 $response->flush();
+
+
