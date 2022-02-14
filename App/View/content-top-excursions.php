973883f0a9947ce201f6c74aac701681a610900f
maik791277 <67161648+maik791277@users.noreply.github.com>
2022-02-15 01:36:44 +0200

    добавил новые блоки для content-top-excursions.php

--- App/View/content-top-excursions.php
+++ App/View/content-top-excursions.php
@@ -54,3 +54,23 @@ use App\Lib\Render;
 		<?= Render:: renderContent("content-card",['excursions'=>$excursions])?>
 	</div>
 </div>
+<div class="bloc3-difference">
+	<div style="display: flex;justify-content: flex-end;">
+		<img style="" src="/Resources/Images/happy-retired-couple-enjoying-nature-in-the-californian-forest 3.png">
+	</div>
+	<div class="difference-text">
+		<p class="difference-text-main">Что отличает эти поездки от других?</p>
+		<p class="difference-text-secondary">Мы считаем, что отпуск должен быть чем-то большим, чем номер в отеле, перелет и прокат автомобиля. Оно должно быть больше, чем сумма его частей. Мы также верим, что вызов может помочь вам расти, а поездка может всколыхнуть душу. Мы создаем путешествия, которые стоит совершить - для путешественника, для принимающей стороны и для всего мира.</p>
+	</div>
+</div>
+<div class="bloc4-map">
+	<div>
+		<img src="/Resources/Images/карта бэг 1.png">
+	</div>
+	<div class="bloc4-map-text">
+		<p class="map-text-main">КУДА МОЖНО ОТПРАВИТЬСЯ?</p>
+		<p class="map-text-secondary">Откройте для себя мир!</p>
+		<p class="map-text-secondary-2"> Начать приключение просто — выбери интересное направление и отправь заявку организатору. Цена указана за одного человека. Приятного отдыха и ярких впечатлений!</p>
+		<a>Посмотреть экскурсии</a>
+	</div>
+</div>
