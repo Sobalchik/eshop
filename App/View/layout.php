bab7f6d078014a254a8480db5fa988725b13a99c
Krupin Mikhail <krupin732@yandex.ru>
2022-02-15 08:21:10 +0200

    Перенос ресета в layout.php

--- App/View/layout.php
+++ App/View/layout.php
@@ -9,9 +9,9 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=0.8, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Title</title>
-
-	<link rel="stylesheet" type="text/css" href="/Resources/CSS/style.css">
 	<link rel="stylesheet" type="text/css" href="/Resources/CSS/Reset.css">
+	<link rel="stylesheet" type="text/css" href="/Resources/CSS/style.css">
+
 </head>
 <body>
 <div class="header">
