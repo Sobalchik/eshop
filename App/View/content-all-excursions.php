d9223113f283641727ddf4904e56a1b30e9e35f5
Krupin Mikhail <krupin732@yandex.ru>
2022-02-15 09:02:04 +0200

    Исправлен хард код Добавлено получение конечной страницы из базы данных Теперь кнопка не появляеться если нет следующей страницы Добавлен JS который сохронаяет прокрутку страницы

--- App/View/content-all-excursions.php
+++ App/View/content-all-excursions.php
@@ -51,18 +51,30 @@ use App\Lib\Render;
 	</div>
 </div>
 <div class="bloc2">
-	<button>asda</button>
 	<div class="bloc-2-contener">
 		<?= Render:: renderContent("content-card",['excursions'=>$excursions])?>
 		<div style="display: flex;justify-content: space-between;">
+
+			<?php if($page > 1){ ?>
 			<a class="cta" href="http://eshop/allExcursions/<?= ($page-1) > 1 ?($page-1): 1 ?>">
 				<img src="/Resources/Images/icons8-стрелка,-указывающая-влево-30.png">
 				<span>назад</span>
 			</a>
-			<a href="http://eshop/allExcursions/<?= ($page+1) < $pageCount ?($page+1): $pageCount ?>">?>">
-				<span>вперед</span>
+			<?php } ?>
+
+			<?php if($page < $pageCount){ ?>
+				'<a class="cta" href="http://eshop/allExcursions/<?= ($page+1) < $pageCount ?($page+1): $pageCount ?>">
 				<img src="/Resources/Images/icons8-длинная-стрелка-вправо-30.png">
-			</a>
+				<span>вперед</span>
+				</a>'
+			<?php } ?>
+
 		</div>
 	</div>
 </div>
+<script>
+	let cords = ['scrollX','scrollY'];
+	// Перед закрытием записываем в локалсторадж window.scrollX и window.scrollY как scrollX и scrollY
+	window.addEventListener('unload', e => cords.forEach(cord => localStorage[cord] = window[cord]));
+	// Прокручиваем страницу к scrollX и scrollY из localStorage (либо 0,0 если там еще ничего нет)
+	window.scroll(...cords.map(cord => localStorage[cord]));</script>
