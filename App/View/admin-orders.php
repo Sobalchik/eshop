de88cbaf3f06ed8c93291d994451fff3b28f2fa5
maik791277 <67161648+maik791277@users.noreply.github.com>
2022-02-18 10:00:43 +0200

    переделал admin-orders.php

--- App/View/admin-orders.php
+++ App/View/admin-orders.php
@@ -9,22 +9,34 @@
 				<form method="post">
 					<div class="admin-orders-bloc1-form">
 						<div class="admin-orders-bloc1-clom1">
-							<p class="admin-orders-text">ФИО</p>
-							<input class="inpit-me form-control" type="text">
-							<p class="admin-orders-text">Номер</p>
-							<input class="inpit-me form-control" type="text">
+							<div style="display: flex;flex-direction: column;align-items: center;">
+								<p class="admin-orders-text">ФИО</p>
+								<p class="inpit-me-order form-control">АБОБА</p>
+							</div>
+							<div style="display: flex;flex-direction: column;align-items: center;">
+								<p class="admin-orders-text">Номер</p>
+								<p class="inpit-me-order form-control">АБОБА</p>
+							</div>
 						</div>
 						<div class="admin-orders-bloc1-clom1">
-							<p class="admin-orders-text">Почта</p>
-							<input class="inpit-me form-control" type="text">
-							<p class="admin-orders-text">Статус</p>
-							<input class="inpit-me form-control" type="text">
+							<div style="display: flex;flex-direction: column;align-items: center;">
+								<p class="admin-orders-text">Почта</p>
+								<p class="inpit-me-order form-control">АБОБА</p>
+							</div>
+							<div style="display: flex;flex-direction: column;align-items: center;">
+								<p class="admin-orders-text">Статус</p>
+								<p class="inpit-me-order form-control">АБОБА</p>
+							</div>
 						</div>
 						<div class="admin-orders-bloc1-clom1">
-							<p class="admin-orders-text">Название.экс</p>
-							<input class="inpit-me form-control" type="text">
-							<p class="admin-orders-text">Дата</p>
-							<input class="inpit-me form-control" type="text">
+							<div style="display: flex;flex-direction: column;align-items: center;">
+								<p class="admin-orders-text">Название.экс</p>
+								<p class="inpit-me-order form-control">АБОБА</p>
+							</div>
+							<div style="display: flex;flex-direction: column;align-items: center;">
+								<p class="admin-orders-text">Дата</p>
+								<p class="inpit-me-order form-control">asd</p>
+							</div>
 						</div>
 						<div class="admin-orders-bloc1-clom2">
 							<input class="admin-excursions-detaild-bloc4-input admin-input-color-2" type="submit" value="save">
