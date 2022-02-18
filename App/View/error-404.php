990a8489f68d2a550e2e4be4b7033e297747ac77
maik791277 <67161648+maik791277@users.noreply.github.com>
2022-02-18 17:02:17 +0200

    добавил error-404.php

--- App/View/error-404.php
+++ App/View/error-404.php
@@ -2,5 +2,20 @@
 
 
 ?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=0.8, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" type="text/css" href="/Resources/CSS/Reset.css">
+<link rel="stylesheet" type="text/css" href="/Resources/CSS/style.css">
 
-<p>asdasdasd</p>
+
+<div class="error-404">
+	<div class="error-404-bloc1">
+		<div>
+			<p class="error-404-p">Ooh shit! - 404</p>
+		</div>
+		<div class="error-404-a">
+			<a href="http://eshop/">вернуться на главную страницу</a>
+		</div>
+	</div>
+</div>
