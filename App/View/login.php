03d748e4e356ce457a7147f5a74702ff47ad7080
Krupin Mikhail <krupin732@yandex.ru>
2022-02-18 10:32:27 +0200

    Заменен путь с /excursion на /admin/excursion

--- App/View/login.php
+++ App/View/login.php
@@ -20,7 +20,7 @@
 					<div class="card-body p-5 text-center">
 
 						<div class="mb-md-5 mt-md-4 pb-5">
-							<form action="/excursions" method="post">
+							<form action="/admin/excursions" method="post">
 
 								<h2 class="fw-bold mb-2 text-uppercase">Login</h2>
 								<p class="text-white-50 mb-5">Please enter your login and password!</p>
