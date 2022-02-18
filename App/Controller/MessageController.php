8aac67948e81c31ced4907d5706adf10b752b2c5
Krupin Mikhail <krupin732@yandex.ru>
2022-02-18 16:43:04 +0200

    Добавле контроллер для обработки сообщений

---
+++ App/Controller/MessageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use App\Lib\Render;
+
+class MessageController
+{
+	public static function showErrorPage()
+	{
+
+		return Render::renderContent('error-404');
+	}
+
+
+}
