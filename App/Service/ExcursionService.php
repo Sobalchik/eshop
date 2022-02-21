e80d70e9f2cd288fd0170e739397e3c79a11f84d
Krupin Mikhail <krupin732@yandex.ru>
2022-02-21 13:28:31 +0200

    Исправлена ошибка во время merge

--- App/Service/ExcursionService.php
+++ App/Service/ExcursionService.php
@@ -369,7 +369,7 @@ class ExcursionService
 		$active = $excursion->getActive();
 
 		$stmt = mysqli_prepare($db, $query);
-		mysqli_stmt_bind_param($stmt,"sssiddddiisiii",
+		mysqli_stmt_bind_param($stmt,"ssiddddiisiii",
 								$nameCity,
 								$nameCountry,
 								$price,
