f3c11995b8a7b31f1cae6623cacdd81b1f42306b
Dmitrii Sobinin <Sobalchik@yandex.ru>
2022-02-22 17:57:35 +0200

    Пагинация работает на всех необходимых страницах

--- App/View/content-all-excursions.php
+++ App/View/content-all-excursions.php
@@ -27,5 +27,4 @@ use App\Lib\Render;
 	window.scroll(...cords.map(cord => localStorage[cord]));</script>
 
 	<script type="text/javascript" async src="/Resources/JS/pagination.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
