3f7ef272306a496e04f9e80d7c54069cec1294d1
user <procsitehcom2009@yandex.ru>
2022-02-26 19:36:05 +0200

    Метод для получения всех тэгов

--- App/Controller/TagController.php
+++ App/Controller/TagController.php
@@ -11,14 +11,20 @@ class TagController
 {
 	public static function showAdminTagsPrepare(): string
 	{
+		$typeTags = self::getAllTagsAction();
+		$content = Render::renderContent("admin-tags-list", ["typeTags" => $typeTags]);
+		return $content;
+	}
+
+	public static function getAllTagsAction(): array
+	{
 		$typeTags = TagService::getTypeTagsForAdminPage(Database::getDatabase());
 		foreach ($typeTags as $typeTag)
 		{
 			$tagsBelong = TagService::getTagsForAdminPage(Database::getDatabase(), $typeTag->getId());
 			$typeTag->setTagsBelong($tagsBelong);
 		}
-		$content = Render::renderContent("admin-tags-list", ["typeTags" => $typeTags]);
-		return $content;
+		return $typeTags;
 	}
 
 	public static function showAdminTags(): string
